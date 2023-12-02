811a197e6975d1d60ba2871a8e93e42344dac4de
farhad <mahdi.as.2537@gmail.com>
2023-12-02 10:04:39 +0330

    Add Blog application and requirements.txt -- 2

--- FootballBlog/settings.py
+++ FootballBlog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=1r^i$cxfzu9+kp7s3lzi!=6p$yjf9vubbfyti4uox)v6v+_7l'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
