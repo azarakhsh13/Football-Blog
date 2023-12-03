e0ba1d4f8d3ea7b7cffaca4db0cd13752d1c27da
farhad <mahdi.as.2537@gmail.com>
2023-12-03 19:57:21 +0330

    Add Author and Post models, and configure admin panel settings for them.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Author, Post
+
 
 # Register your models here
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('date',)
+
+
+admin.site.register(Author)
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator, MinLengthValidator
+
 
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=40, blank=False, validators=[MaxLengthValidator(40)])
+    last_name = models.CharField(max_length=40, blank=False, validators=[MaxLengthValidator(40)])
+    national_id = models.CharField(unique=True, max_length=10,
+                                   validators=[MaxLengthValidator(10), MinLengthValidator(10)],
+                                   blank=False)
+
+
+class Post(models.Model):
+    author = models.ManyToManyField(Author)
+    to_show = models.BooleanField(blank=False, default=False)
+    text = models.TextField(blank=False)
+    image = models.ImageField(upload_to='Images/', blank=True)
+    video = models.FileField(upload_to='Videos/', blank=True)
+    date = models.DateTimeField(auto_now=True)
