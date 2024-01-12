17f6f607f6631db7909c469985712a0e0933f550
omidparsa <omidparsa13.iran@gmail.com>
2024-01-12 13:12:51 +0330

    databaseconfig

--- FootballBlog/settings.py
+++ FootballBlog/settings.py
@@ -78,10 +78,14 @@ WSGI_APPLICATION = 'FootballBlog.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'irancoach',
+       'USER': 'postgres',
+       'PASSWORD': 'parsa2558bahram',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 # Password validation
